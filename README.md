4aea4c428cd1ec446fbb7a9df72b880e546593f9
Warren Guy <warren@guy.net.au>
2015-06-25 22:04:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Docker container memory usage plugin for munin
+
+This plugin charts memory uage of all running docker containers in one
+chart.
+
+## Requirements:
+
+* Munin
+* Docker
+* Ruby
+
+## Configuration
+
+Root is required to access docker's statistics. Add to your munin-node
+plugin configuration:
+
+```
+[docker_containers]
+user root
+```
+
+## License
+
+MIT license. See LICENSE.
+
+## Author
+
+Warren Guy <warren@guy.net.au>
+
+https://warrenguy.me
---
+++ docker_containers.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+docker_ps = `docker ps --no-trunc`.split("\n")
+docker_ps.shift
+
+containers = {}
+docker_ps.each do |container|
+  id        = container.split(' ').first
+  name      = container.split(' ').last.split(',').first
+  mem_bytes = File.read(open("/sys/fs/cgroup/memory/docker/#{id}/memory.usage_in_bytes")).to_i
+
+  containers[id] = {name: name, mem_bytes: mem_bytes}
+end
+
+case ARGV[0]
+when 'config'
+  puts ({
+    'graph_title'            => 'Docker container memory uage',
+    'graph_vlabel'           => 'bytes',
+    'graph_category'         => 'docker_container',
+  }).map{|k,v| [k,v].join(" ")}.join("\n")
+  containers.each do |id, container|
+    display_name = container[:name].gsub(/[^0-9a-z ]/i, '_')
+    puts "#{display_name}.label #{container[:name]}"
+  end
+else
+  containers.each do |id, container|
+    display_name = container[:name].gsub(/[^0-9a-z ]/i, '_')
+    puts "#{display_name}.value #{container[:mem_bytes]}"
+  end
+end
